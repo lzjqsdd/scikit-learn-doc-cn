f2156a22d06537eecc5f32d13ee66e8ca123862e
lzjqsdd <lzj7179@163.com>
2017-03-12 11:09:08 +0800

    update

--- install.sh
+++ install.sh
@@ -61,9 +61,9 @@ python --version
 python -c "import numpy; print('numpy %s' % numpy.__version__)"
 python -c "import scipy; print('scipy %s' % scipy.__version__)"
 
-- cp -r translate/* doc
-- cd doc
-- travis-sphinx --source=. --nowarn build
-- cd target/doc/build
-- echo "sklearn.lzjqsdd.com" > CNAME
-- cd ../../..
+cp -r translate/* doc
+cd doc
+travis-sphinx --source=. --nowarn build
+cd target/doc/build
+echo "sklearn.lzjqsdd.com" > CNAME
+cd ../../..
